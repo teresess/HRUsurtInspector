8055357257cfcc1ca9387289967bb75ca7a0ec01
teresess <sem.tatarinov@icloud.com>
2025-11-20 20:11:26 +0500

    Mini update
    Added JpaConfig, CorsConfig
    
    Repository folder and service folder

---
+++ src/main/java/upd/dev/usurtprojecthr/config/CorsConfig.java
@@ -0,0 +1,16 @@
+package upd.dev.usurtprojecthr.logistic.config;//package upd.dev.usurtratings.logistic.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/api/**")
+//                .allowedOrigins("http://103.249.134.228:3000")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                .allowedHeaders("*");
+//    }
+//}
